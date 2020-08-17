39e1242232398ed1c3687c326e46b6157b4d59e3
nedretb <nedret.jr@gmail.com>
2020-08-18 01:44:58 +0200

    esfs

--- elementsByTagName/script.js
+++ elementsByTagName/script.js
@@ -1,7 +1,18 @@
 function byTagName(node, tagName) {
-    if (node.nodeType == Node.ELEMENT_NODE){
-        
+  let solution = [];
+  tagName = tagName.toUpperCase();
+
+  function explore(node){
+    for(let i = 0; i < node.childNodes.length; i++){
+      let child = node.childNodes[i];
+      if(child.nodeType == document.ELEMENT_NODE){
+        if(child.nodeName == tagName) solution.push(child);
+        explore(child);
+      }
+    }
   }
+  explore(node);
+  return solution;
 }
   console.log(byTagName(document.body, "h1").length);
   // → 1
